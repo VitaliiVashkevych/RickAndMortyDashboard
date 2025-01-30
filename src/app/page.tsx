1ae55ff31ed62e11eea2f79dbcffa2e5b4723dd9
Vitalii Vashkevych <NuclearSunStar@gmail.com>
2025-01-30 23:02:21 +0200

    Skeletons for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setCharacters } from "@/redux/reducers";
 import Link from "next/link";
 import { gql, useQuery } from "@apollo/client";
 import { flavors } from "./ui/fonts";
 
 import "@/app/globals.css";
+import { Skeleton } from "./ui/skeletons";
 
 const getCharactersGraph = gql`
   query {
@@ -22,34 +20,31 @@ const getCharactersGraph = gql`
 `;
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const characters = useSelector((state: any) => state.characters.characters);
   const { data, loading, error } = useQuery(getCharactersGraph);
 
-  useEffect(() => {
-    if (data) {
-      dispatch(setCharacters(data.characters.results));
-    }
-  }, [dispatch, data]);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error : {error.message}</p>;
 
   return (
     <main className="flex flex-col items-center">
       <h1 className={`${flavors.className} text-6xl mt-20 mb-16`}>
         Rick and Morty Characters
       </h1>
+
       <ul className="grid md:grid-cols-3 gap-x-20 gap-y-12">
-        {characters &&
-          characters.map((char: any) => (
-            <li key={char.id} className="bg-gray-700 rounded-3xl">
-              <Link href={`/character?id=${char.id}`}>
-                <img src={char.image} alt={char.name} className="rounded-3xl mr-4 ml-4 mt-4" />
-                <h2 className="text-2xl m-4">{char.name}</h2>
-              </Link>
-            </li>
-          ))}
+        {loading && Array(9).fill(1).map(() => <Skeleton />)}
+        {data?.characters?.results?.map((char: any) => (
+          <li key={char.id} className="bg-gray-700 rounded-3xl max-w-[300px]">
+            <Link href={`/character?id=${char.id}`}>
+              <img
+                src={char.image}
+                alt={char.name}
+                className="rounded-3xl p-4 box-border"
+              />
+              <h2 className="text-2xl p-4">{char.name}</h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </main>
   );
---
+++ src/app/ui/skeletons.tsx
@@ -0,0 +1,10 @@
+import './skeletons.css';
+
+export const Skeleton = () => {
+  return (
+    <div className="rect skeleton-content">
+      <div className='skeleton-image'></div>
+      <div className='skeleton-title'></div>
+    </div>
+  )
+}
