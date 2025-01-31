a80ae7b2b473f3b018cf5e69dfd53c8e6eb6fdd1
Vitalii Vashkevych <NuclearSunStar@gmail.com>
2025-01-31 14:46:51 +0200

    Added pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ import { gql, useQuery } from "@apollo/client";
 import { flavors } from "./ui/fonts";
 
 import "@/app/globals.css";
-import { Suspense } from "react";
 import { Skeleton } from "./ui/skeletons";
+import { usePathname, useSearchParams } from "next/navigation";
+import { Pagination } from "@mui/material";
 
-const getCharactersGraph = gql`
+const HomePage = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const getCharactersGraph = gql`
   query {
-    characters(page: 1) {
+    characters(page: ${currentPage}) {
       results {
         id
         name
@@ -19,18 +25,38 @@ const getCharactersGraph = gql`
     }
   }
 `;
-
-const HomePage = () => {
   const { data, loading, error } = useQuery(getCharactersGraph);
 
+  const handlePage = (e: any) => {
+    e.preventDefault();
+    const pageNumber = e.target.innerText;
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    window.location.href = `${pathname}?${params.toString()}`;
+    return `${pathname}?${params.toString()}`;
+  };
+
   // if (loading) return <Skeleton />;
-  // if (error) return <p>Error : {error.message}</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
   return (
     <main className="flex flex-col items-center">
       <h1 className={`${flavors.className} text-6xl mt-20 mb-16`}>
         Rick and Morty Characters
       </h1>
+
+      <div className="bg-white mb-12">
+        <Pagination
+          count={42}
+          defaultPage={1}
+          page={currentPage}
+          siblingCount={2}
+          color="primary"
+          onChange={handlePage}
+          hidePrevButton
+          hideNextButton
+        />
+      </div>
 
       <ul className="grid md:grid-cols-3 gap-x-20 gap-y-12">
         {loading && <Skeleton />}
